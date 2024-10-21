908e44f1af6604b2a4ae30569d7e9d5f56ab1166
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-10-21 09:48:34 +0300

    Router update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AddRoom from './components/rooms/AddRoom';
 import ExistingRooms from './components/rooms/ExistingRooms';
+import Home from './components/home/Home';
+import EditRoom from './components/rooms/EditRoom';
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<div className="App">
-				<AddRoom />
-				<ExistingRooms />
-			</div>
-		</BrowserRouter>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/edit-room/:roomId" element={<EditRoom />} />
+				<Route path="/existing-rooms" element={<ExistingRooms />} />
+				<Route path="/add-room" element={<AddRoom />} />
+			</Routes>
+		</Router>
 	);
 };
 
