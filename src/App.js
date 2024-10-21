f0bf260954e1556400c10344c6a51d297074c247
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-10-21 15:20:40 +0300

    Footer and Navbar added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import AddRoom from './components/rooms/AddRoom';
 import ExistingRooms from './components/rooms/ExistingRooms';
 import Home from './components/home/Home';
 import EditRoom from './components/rooms/EditRoom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 const App = () => {
 	return (
 		<Router>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/edit-room/:roomId" element={<EditRoom />} />
 				<Route path="/existing-rooms" element={<ExistingRooms />} />
 				<Route path="/add-room" element={<AddRoom />} />
 			</Routes>
+			<Footer />
 		</Router>
 	);
 };
